6c2242877e68b16630e11d2c47a547529de61da6
Kbzinho-66 <gvalles@ucs.br>
2021-04-02 21:13:01 -0300

    parseMovies completada, parseDirectors iniciada

--- CatalogoFilmes.cpp
+++ CatalogoFilmes.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
+#include <optional>
+
+#define INVALID_STRING "unknown"
+#define INVALID_NUMBER -1
 
 using std::cout;
 using std::cin;
 using std::endl;
+
 
 class Movie {
 
@@ -12,28 +17,37 @@ class Movie {
         int movieID;
         std::string title;
         int releaseYear;
-        std::string url;
-        int directorID;
-        Director director;
+        std::optional<std::string> url;
+        std::optional<int> directorID;
+        std::optional<Director> director;
 
         Movie(int movieID,
             std::string title,
             int releaseYear,
-            std::string url,
-            int directorID
+            std::optional<std::string> url,
+            std::optional<int> directorID
         ){
             this->movieID = movieID;
             this->title = title;
             this->releaseYear = releaseYear;
             this->url = url;
             this->directorID = directorID;
-            //TODO Definir um diretor nullable https://www.bfilipek.com/2018/05/using-optional.html
-            // :)
+        }
+
+        Movie() {
+        }
+
+        void reset() {
+            movieID = 0;
+            title = "";
+            releaseYear = 0;
+            url = "";
+            directorID = 0;
         }
 
         void printMovie() {
             cout << "Título: " << this->title << " (" << this->releaseYear << ")" << endl;
-            cout << "Diretor: " << this->director.name << endl;
+            cout << "Diretor: " << this->director->name << endl;
         }
 
 
@@ -46,9 +60,96 @@ class Director {
         std::string name;
         std::vector<Movie> moviesDirected;
 
+        Director(int directorID, std::string name) {
+            this->directorID = directorID;
+            this->name = name;
+        }
+
+        Director() {
+
+        }
+
+        void reset() {
+            directorID = 0;
+            name = "";
+        }
+
 };
+
+FILE *gerarArquivoOrganizado();
+
+std::vector<Movie> parseMovies(FILE *moviesFile);
+
+std::vector<Director> parseDirectors(FILE *directors, std::vector<Movie> movies);
 
 int main() {
     
+    std::vector<Movie> movies;
+    std::vector<Director> directors;
+    FILE *moviesFile, *directorsFile;
+
+    moviesFile = fopen("arqMovies.txt", "r");
+
+    if (moviesFile == NULL) {
+        cout << "Erro ao abrir o arquivo dos filmes." << endl;
+        exit(0);
+    } else {
+        movies = parseMovies(moviesFile);
+        fclose(moviesFile);
+    }
+
+    directorsFile = fopen("directors.txt", "r");
+
+    if (directorsFile == NULL) {
+        cout << "Erro ao abrir o arquivo dos diretores. << endl";
+        exit(0);
+    } else {
+        directors = parseDirectors(directorsFile, movies);
+    }
+
     return 0;
+}
+
+std::vector<Movie> parseMovies(FILE *moviesFile) {
+
+    Movie temp = Movie();
+    std::vector<Movie> movies;
+    
+    while (fscanf(moviesFile, "%d;%s;%d;%s;%d", &temp.movieID, temp.title, temp.releaseYear, temp.url, temp.directorID) > 0) {
+
+        // Se não tiver as informações básicas, colocar o filme como inválido para manter a indexação original
+        if (temp.movieID == 0 || temp.title == "" || temp.releaseYear == 0) { 
+            temp.title = INVALID_STRING;
+            temp.releaseYear = INVALID_NUMBER;
+            temp.url = INVALID_STRING;
+            temp.directorID = INVALID_NUMBER;
+            movies.push_back(temp);
+            temp.reset();
+        }
+        movies.push_back(temp);
+        temp.reset();
+        
+    }
+
+    return movies;
+
+}
+
+std::vector<Director> parseDirectors(FILE *directorsFile, std::vector<Movie> movies) {
+
+    Director temp = Director();
+    std::vector<Director> directors;
+    int movieIndex;
+    Movie directedMovie = Movie();
+
+    while (fscanf(directorsFile, "%d,%s", directedMovie, temp.name) > 0) {
+
+        directedMovie = movies.at(movieIndex);
+
+
+        temp.directorID = directedMovie.directorID.value_or(INVALID_NUMBER);
+        temp.moviesDirected.push_back(directedMovie);
+
+        
+    }
 }
