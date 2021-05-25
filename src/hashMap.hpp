ced3d211cbdf928a1b2500d801f36888254197b6
Marcello <marcefabri20@gmail.com>
2021-05-24 23:35:40 -0300

    Hashmap

--- src/hashMap.hpp
+++ src/hashMap.hpp
@@ -12,26 +12,29 @@
 #include <cassert>
 
 #include "DataFileHandler.hpp"
+#include "IndexFileHandler.hpp"
 #include "Registry.hpp"
 
 class HashMap
 {
-    public:
-        std::multimap<std::string, int> map;
-        HashMap();
-        ~HashMap();
-        void insertNewValue(std::string dateKey, int entryPosition);
-        void printAllValuesFromKey(std::string dateKey);
+public:
+    std::multimap<std::string, int> map;
+    HashMap();
+    ~HashMap();
+    void insertNewValue(std::string dateKey, int entryPosition);
+    void printAllValuesFromKey(std::string dateKey);
+    void searchRegistry(int index);
 };
 
-
-HashMap::HashMap() 
+HashMap::HashMap()
 {
-
 }
 
 void HashMap::insertNewValue(std::string dateKey, int entryPosition)
 {
+    //insert já verifica se valor chave já está na lista.
+    //se está, insere no conjunto dos valores, se não insere
+    //nova chave com o valor
     map.insert(std::pair<std::string, int>(dateKey, entryPosition));
 }
 
@@ -50,21 +53,43 @@ void HashMap::printAllValuesFromKey(std::string dateKey)
 
     std::multimap<std::string, int>::iterator it;
 
-    if(map.find(dateKey) == map.end())
+    if (map.find(dateKey) == map.end())
     {
         std::cout << "Data não encontrada" << std::endl;
         return;
     }
 
-    for (it = map.begin(); it != map.end(); it++) 
+    for (it = map.begin(); it != map.end(); it++)
     {
-        if(it -> first == dateKey)
+        if (it->first == dateKey)
         {
-            int address = it -> second;
-            Registry::Registry registry = dataFile->getRegistryByAddress(address);
-
-            registry.printRegistryInfo();
+           getRegistry(it->second);
         }
     }
 }
 
+void HashMap::searchRegistry(int index)
+{
+    std::multimap<std::string, int>::iterator it;
+
+    for (it = map.begin(); it != map.end(); it++)
+    {
+        if (it->second == index)
+        {
+            getRegistry(index);
+            return;
+        }
+    }
+
+    cout << "Código não encontrado." << endl
+         << endl;
+}
+
+void getRegistry(int index)
+{
+    //abre arquivo da cidade
+    DataFile *dataFile = new DataFile();
+    CodeIndex *codeIndex = new CodeIndex(*dataFile);
+    //procura o indice
+    codeIndex->searchRegistryByCode(index);
+}
